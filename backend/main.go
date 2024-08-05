68181ceedfd893a1fc69931c82047f81ea17507b
rizmulya <riskymulya@protonmail.com>
2024-08-05 22:49:56 +0700

    chore: react setup

--- backend/main.go
+++ backend/main.go
@@ -12,5 +12,21 @@ func main() {
         return c.JSON(fiber.Map{"message": "hi welcome"})
     })
 
+    // frontend
+    frontendRoutes := map[string]bool{
+        "/about": true,
+        "/contact": true,
+        "/service": true,
+    }
+
+    app.Use(func(c *fiber.Ctx) error {
+        if _, exists := frontendRoutes[c.Path()]; exists {
+            return c.SendFile("./frontend/dist/index.html")
+        }
+        return c.Next()
+    })
+
+    app.Static("/", "./frontend/dist")
+
     app.Listen(":8000")
 }
